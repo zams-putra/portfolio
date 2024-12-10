6f98eb2e7d0f0f9f394355dff35535b7ad2598d0
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-10 12:21:38 +0700

    add: current song spotify

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,35 @@ import AboutMe from "./components/AboutMe";
 import TechStack from "./components/TechStack";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import axios from "axios";
 
 function App() {
   const [notSplash, setNotSplash] = useState(false);
   const { scrollYProgress } = useScroll();
+  const [lagu, setLagu] = useState({});
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setNotSplash(true);
     }, 5000);
     return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    const getLagu = async () => {
+      try {
+        const res = await axios.get(
+          "https://quizmaker-app-api.vercel.app/api/lagu_spotify"
+        );
+
+        const data = await res.data;
+        setLagu(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getLagu();
   }, []);
 
   return (
@@ -31,18 +50,48 @@ function App() {
           <Projects />
           <Experience />
 
-          <section className="w-full h-screen flex justify-center items-center">
+          <section className="w-full h-screen flex  flex-col justify-center items-center">
             <motion.div
               style={{
                 scaleX: scrollYProgress,
               }}
               className="fixed top-0 w-full h-1 bg-green-400"
             ></motion.div>
+
+            {lagu.artist ? (
+              <motion.div
+                initial={{ x: 300 }}
+                whileInView={{ x: 0 }}
+                transition={{ duration: 1 }}
+                className="w-full h-1/2 p-4 flex flex-col items-center gap-4"
+              >
+                <p className="text-xs md:text-xl bg-gradient-to-r from-slate-500 to-slate-200 bg-clip-text text-transparent">
+                  Putro sedang mendengarkan ini sekarang
+                </p>
+                <p className="text-xs md:text-xl bg-gradient-to-r from-slate-500 to-slate-200 bg-clip-text text-transparent">
+                  {lagu.judul} - {lagu.artist}
+                </p>
+                <img
+                  className="w-32 h-32 rounded-md border-green-400 border-2"
+                  src={lagu.imgLagu}
+                  alt={lagu.judul}
+                />
+              </motion.div>
+            ) : (
+              <motion.p
+                className="text-xs md:text-xl bg-gradient-to-r from-slate-500 to-slate-200 bg-clip-text text-transparent"
+                initial={{ x: 300 }}
+                whileInView={{ x: 0 }}
+                transition={{ duration: 1 }}
+              >
+                {lagu.message}
+              </motion.p>
+            )}
             <motion.h1
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               transition={{ duration: 1, delay: 0.2 }}
-              className="text-2xl md:text-4xl bg-gradient-to-r from-slate-500 to-slate-200 bg-clip-text text-transparent"
+              className="text-2xl h-1/2 md:text-4xl bg-gradient-to-r from-slate-500 to-slate-200 bg-clip-text text-transparent"
             >
               Thank you :)
             </motion.h1>
