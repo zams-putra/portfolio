eb3e36a7ce34ee3fdbfb90c3ba07e8b7f757d858
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-02-17 09:01:52 +0700

    add: baru

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "motion/react";
+import { AnimatePresence, motion, useScroll } from "motion/react";
 import { useEffect, useState } from "react";
 
 import SplashScreen from "./components/SplashScreen";
@@ -17,24 +17,7 @@ const sesi = [Hero, AboutMe, TechStack, Projects, Experience, Social, End]
 function App() {
 
   const [page, setPage] = useState(0)
-  const [isScroll, setIsScroll] = useState(false)
-
-
-  const handleScroll = (e) => {
-    if (isScroll) return
-    setIsScroll(true)
-
-    if (e.deltaY > 0 && page < sesi.length - 1) {
-      setPage((bef) => bef + 1)
-    } else if (e.deltaY < 0 && page > 0) {
-      setPage((bef) => bef - 1)
-    }
-
-    setTimeout(() => {
-      setIsScroll(false)
-    }, 800)
-
-  }
+  const { scrollYProgress } = useScroll()
 
   const handleDown = () => {
     if (page < sesi.length - 1) {
@@ -47,16 +30,6 @@ function App() {
     }
   }
 
-
-
-  useEffect(() => {
-    window.addEventListener("wheel", handleScroll)
-
-    return () => {
-      window.removeEventListener("wheel", handleScroll)
-
-    }
-  })
 
   const Curr = sesi[page]
 
@@ -81,39 +54,48 @@ function App() {
 
       {notSplash && (
         <>
+          <main className="md:hidden">
+            <AnimatePresence mode="wait" key={page}
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 2 }}
 
-
-
-          <AnimatePresence mode="wait" key={page}
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -50 }}
-            transition={{ duration: 2 }}
-
-          >
-            <section className="py-8 justify-center w-full items-center flex flex-col">
-              <motion.div
-                initial={{ y: -200 }}
-                animate={{ y: 0 }}
-                transition={{ duration: 1 }}
-                className="lg:hidden p-4 gap-4 flex">
-                <button onClick={handleUp} className="px-5 bg-slate-700 text-slate-100 rounded-md">Up</button>
-                <button onClick={handleDown} className="px-5 bg-slate-700 text-slate-100 rounded-md">Down</button>
-              </motion.div>
-              <Curr />
-            </section>
-          </AnimatePresence>
-
-
-
-
-
-          <motion.div
-            style={{
-              scaleX: `${(page + 1) / sesi.length * 100}%`,
-            }}
-            className="fixed top-0 w-full h-1 bg-green-400"
-          ></motion.div>
+            >
+              <section className="py-8 justify-center w-full items-center flex flex-col">
+                <motion.div
+                  initial={{ y: -200 }}
+                  animate={{ y: 0 }}
+                  transition={{ duration: 1 }}
+                  className="lg:hidden p-4 gap-4 flex">
+                  <button onClick={handleUp} className="px-5 bg-slate-700 text-slate-100 rounded-md">Up</button>
+                  <button onClick={handleDown} className="px-5 bg-slate-700 text-slate-100 rounded-md">Down</button>
+                </motion.div>
+                <Curr />
+              </section>
+            </AnimatePresence>
+            <motion.div
+              style={{
+                scaleX: `${(page + 1) / sesi.length * 100}%`,
+              }}
+              className="fixed md:hidden top-0 w-full h-1 bg-green-400"
+            ></motion.div>
+          </main>
+          <main className="flex-col gap-2 hidden lg:flex">
+            <Hero />
+            <AboutMe />
+            <TechStack />
+            <Projects />
+            <Experience />
+            <Social />
+            <End />
+            <motion.div
+              style={{
+                scaleX: scrollYProgress,
+              }}
+              className="fixed hidden md:block top-0 w-full h-1 bg-green-400"
+            ></motion.div>
+          </main>
 
         </>
       )}
