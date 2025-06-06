5545cd4cd3dc827e1d30a0aa9e7d665f3b57d839
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-06-06 20:52:13 +0700

    add: new lah

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,5 +1,45 @@
 import { motion } from "motion/react";
 
+
+const socials = [
+  {
+    id: 1,
+    link: 'https://github.com/zams-putra',
+    lay: 0.5,
+    name: 'Github'
+  },
+  {
+    id: 2,
+    link: 'https://leetcode.com/u/Sebasers',
+    lay: 0.7,
+    name: 'Leetcode'
+  },
+  {
+    id: 3,
+    link: 'https://www.codewars.com/users/AwikwokBas',
+    lay: 0.9,
+    name: 'CodeWars'
+  },
+  {
+    id: 4,
+    link: 'https://tryhackme.com/r/p/TombaHK',
+    lay: 1.1,
+    name: 'Tryhackme'
+  },
+  {
+    id: 5,
+    link: 'https://medium.com/@sirsebasers',
+    lay: 1.3,
+    name: 'Medium'
+  },
+  {
+    id: 6,
+    link: 'https://instagram.com/username.gw.itu.jir',
+    lay: 1.5,
+    name: 'Instagram'
+  },
+]
+
 export default function Social() {
   return (
     <section className="w-full min-h-screen justify-center overflow-y-hidden flex flex-col items-center gap-8 py-16">
@@ -12,74 +52,24 @@ export default function Social() {
         We can be friends
       </motion.h1>
       <div className="w-full gap-8 items-center md:items-start min-h-screen flex p-8 flex-col md:flex-row flex-wrap">
-        <motion.a
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, rotate: [30, -30, 0] }}
-          transition={{ duration: 1, delay: 0.5 }}
-          viewport={{ once: true }}
-          className="p-4 w-1/2 duration-300 hover:bg-slate-900 hover:text-slate-200 rounded-sm flex items-center justify-center md:w-32 h-10 bg-slate-100 text-slate-900"
-          href="https://github.com/zams-putra"
-          target="_blank"
-        >
-          Github
-        </motion.a>
-        <motion.a
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, rotate: [30, -30, 0] }}
-          transition={{ duration: 1, delay: 0.6 }}
-          viewport={{ once: true }}
-          className="p-4 w-1/2 duration-300 hover:bg-slate-900 hover:text-slate-200 rounded-sm flex items-center justify-center md:w-32 h-10 bg-slate-100 text-slate-900"
-          href="https://leetcode.com/u/Sebasers/"
-          target="_blank"
-        >
-          LeetCode
-        </motion.a>
-        <motion.a
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, rotate: [30, -30, 0] }}
-          transition={{ duration: 1, delay: 0.7 }}
-          viewport={{ once: true }}
-          className="p-4 w-1/2 duration-300 hover:bg-slate-900 hover:text-slate-200 rounded-sm flex items-center justify-center md:w-32 h-10 bg-slate-100 text-slate-900"
-          href="https://www.codewars.com/users/AwikwokBas"
-          target="_blank"
-        >
-          CodeWars
-        </motion.a>
 
-        <motion.a
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, rotate: [30, -30, 0] }}
-          transition={{ duration: 1, delay: 0.9 }}
-          viewport={{ once: true }}
-          className="p-4 w-1/2 duration-300 hover:bg-slate-900 hover:text-slate-200 rounded-sm flex items-center justify-center md:w-32 h-10 bg-slate-100 text-slate-900"
-          href="https://tryhackme.com/r/p/TombaHK"
-          target="_blank"
-        >
-          TryHackMe
-        </motion.a>
+        {socials.map((social) => (
 
-        <motion.a
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, rotate: [30, -30, 0] }}
-          transition={{ duration: 1, delay: 1.1 }}
-          viewport={{ once: true }}
-          className="p-4 w-1/2 duration-300 hover:bg-slate-900 hover:text-slate-200 rounded-sm flex items-center justify-center md:w-32 h-10 bg-slate-100 text-slate-900"
-          href="https://medium.com/@sirsebasers"
-          target="_blank"
-        >
-          Medium
-        </motion.a>
-        <motion.a
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, rotate: [30, -30, 0] }}
-          transition={{ duration: 1, delay: 1.1 }}
-          viewport={{ once: true }}
-          className="p-4 w-1/2 duration-300 hover:bg-slate-900 hover:text-slate-200 rounded-sm flex items-center justify-center md:w-32 h-10 bg-slate-100 text-slate-900"
-          href="https://instagram.com/username.gw.itu.jir"
-          target="_blank"
-        >
-          Instagram
-        </motion.a>
+          <motion.a key={social.id}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, rotate: [30, -30, 0] }}
+            transition={{ duration: 1, delay: social.lay }}
+            viewport={{ once: true }}
+            className="p-4 px-8 w-48 duration-300 hover:bg-slate-900 hover:text-slate-200 rounded-sm flex items-center justify-center md:w-32 h-10 bg-slate-100 text-slate-900"
+            href={social.link}
+            target="_blank"
+          >
+            {social.name}
+          </motion.a>
+
+        ))}
+
+
       </div>
     </section>
   );
